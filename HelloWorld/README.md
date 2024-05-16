25fdd3e705d984ca085e34209fa5a220c0a150f3
miguelfAndrade <miguelandrade.96.18@gmail.com>
2024-05-16 19:24:52 +0100

    first commit

---
+++ HelloWorld/README.md
@@ -0,0 +1,42 @@
+# Configure opengl and glfw in linux (wsl fedora)
+
+sudo dnf install git cmake g++ python
+
+sudo dnf group install "C Development Tools and Libraries" "Development Tools"
+
+Follow this tutorial - https://en.wikibooks.org/wiki/OpenGL_Programming/Installation/Linux
+sudo dnf install glew-devel SDL2-devel SDL2_image-devel glm-devel freetype-devel
+
+You can install the X11 dependencies and/or Wayland dependencies (In the GLFW site says that it does not need this libraries to build or run programs that use GLFW)
+
+sudo dnf install libXcursor-devel libXi-devel libXinerama-devel libXrandr-devel - X11
+sudo dnf install wayland-devel libxkbcommon-devel wayland-protocols-devel extra-cmake-modules - Wayland
+
+Go to the GLFW github and clone the repository
+git clone https://github.com/glfw/glfw.git
+
+cmake -S glfw -B glfw/build -D BUILD_SHARED_LIBS=ON - this is important so that the GLFW is build as a shared library
+
+cd glfw/build and run make command
+
+sudo make install
+
+You may have to add the directory where the libglfw.so is to the environment variable $LD_LIBRARY_PATH, in my case was /usr/local/lib64/
+
+I'am using Visual Studio Code with the extensions C/C++ and C/C++ Extension Pack.
+
+Add to your task.json under args, this "-lglfw", "-lGLU", "-lGL". You can try to run this in the terminal aswell
+
+Install glew - https://glew.sourceforge.net/install.html
+
+git clone https://github.com/nigels-com/glew.git
+
+cd glfw
+
+make
+
+sudo make install
+
+add "-lGLEW" in the task.json or in the g++ command
+
+
---
+++ HelloWorld/src/Main.cpp
@@ -0,0 +1,80 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+#include <iostream>
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) 
+{
+    
+}
+
+
+int main()
+{
+    // install git, cmake, g++
+    // https://www.pragmaticlinux.com/2022/05/how-to-install-build-essential-on-fedora/ - dependencies
+    // install this - sudo dnf install glew-devel SDL2-devel SDL2_image-devel glm-devel freetype-devel - https://en.wikibooks.org/wiki/OpenGL_Programming/Installation/Linux
+    // https://stackoverflow.com/questions/17768008/how-to-build-install-glfw-3-and-use-it-in-a-linux-project/17772217#17772217 - install glfw with the flag shared libraries on
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK){
+        std::cout << "GLEW ERROR!" << std::endl; 
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+    glEnableVertexAttribArray(0);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // glBegin(GL_TRIANGLES);
+        // glVertex2f(-0.5f,-0.5f);
+        // glVertex2f(0.0f,  0.5f);
+        // glVertex2f(0.5f, -0.5f);       
+        // glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
